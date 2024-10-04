18006c2ef3cee74b128376dec0127cd56b0b6c5a
Nathan Dalton <nd221@ic.ac.uk>
2024-10-04 14:18:56 +0100

    Implement removal of item with no parents for chart.

--- src/chart.cpp
+++ src/chart.cpp
@@ -1,5 +1,6 @@
 #include "chart.h"
 #include "item.h"
+#include <vector>
 
 void Chart::add_item() {
     add_item(get_item_from_user());
@@ -28,8 +29,32 @@ void Chart::add_item(const Item _item) {
     }
 }
 
-void Chart::remove_item(const Item* _item) {}
+void Chart::remove_item(const Item* _item) {
+    std::vector<Item*> parents;
+    
+    if (_item->parents.empty()) {
+        for (auto child_ptr {std::begin(_item->children)}; child_ptr != std::end(_item->children); ++child_ptr) {
+            parents.clear();
+            for (auto parent_ptr {std::begin((*child_ptr)->parents)}; parent_ptr != std::end((*child_ptr)->parents); ++parent_ptr) {
+                if (*parent_ptr != _item) {
+                    parents.push_back(*parent_ptr);
+                }
+            }
+            (*child_ptr)->parents = parents;
+            items.push_back(*child_ptr);
+        }
+    }
+    else {
+        // what is this going to do?
+        // for each child node, replace the _item element in parent with all parents of _item
+        // likewise for each parent node with all children of _item
+        // remove 
+    }
+    
+    delete _item;
+    update_levels();
+}
 
 void Chart::update_levels() {
-    // bfs through to find
+    // bfs through and update heirarchical level variable
 }
