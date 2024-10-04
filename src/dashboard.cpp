4a6080cb9dc284e11b731ebe2b3ce3fad351577b
Nathan Dalton <nd221@ic.ac.uk>
2024-10-04 14:19:49 +0100

    Cleanup pure virtual function definitions.

--- src/dashboard.cpp
+++ src/dashboard.cpp
@@ -1,12 +1,6 @@
 #include "dashboard.h"
 #include "item.h"
 
-/* void Dashboard::add_item() {} */
-
-/* void Dashboard::add_item(const Item _item) {} */
-
-/* void Dashboard::remove_item(const Item* _item) {} */
-
 Item Dashboard::get_item_from_user() const {
     return Item {};
 }
